16456c57ad5f66f05c663167bac723697837e601
MayukhDg <mayukh1995@gmail.com>
2022-10-17 15:46:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import ExerciseDetail from "./pages/ExerciseDetail";
 import Footer from "./components/Footer";
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/exercise/:id" element={<ExerciseDetail/>}/>
       </Routes>
       <Footer/>
     </Box>
